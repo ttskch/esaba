f118772722822a95bcaacf15489300cccaafead4
Takashi Kanemoto <kanemoto.takashi@gmail.com>
2017-10-16 15:31:34 +0900

    fix styles, syntax highlighting and something

---
+++ web/js/esa-content.js
@@ -0,0 +1,6 @@
+$(function () {
+    $('#esa-content-body .code-block pre.plaintext code').addClass('nohighlight');
+    hljs.initHighlightingOnLoad();
+
+    $('#esa-content-body table').addClass('table table-bordered table-striped');
+});
