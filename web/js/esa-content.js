b78ec19e25ff920976a5b5f9f4e58a5c73026903
Takashi Kanemoto <kanemoto.takashi@gmail.com>
2017-10-16 20:03:43 +0900

    update style

--- web/js/esa-content.js
+++ web/js/esa-content.js
@@ -2,5 +2,14 @@ $(function () {
     $('#esa-content-body .code-block pre.plaintext code').addClass('nohighlight');
     hljs.initHighlightingOnLoad();
 
-    $('#esa-content-body table').addClass('table table-bordered table-striped');
+    $('#esa-content-body table')
+        .addClass('table table-bordered table-striped')
+        .each(function () {
+            // decrease font-size when the number of column is 5 or more
+            var tr = $(this).find('tr').get(0);
+            if ($(tr).find('td, th').length >= 5) {
+                $(this).css('font-size', '0.9em');
+            }
+        })
+    ;
 });
